35900046a12bcea5e95da335b05f199d8afe9c4b
Muhammed Hussein Karimi <info@karimi.dev>
2024-12-12 14:10:44 +0330

    :sparkles: Isolating tmpdir and gopath in nix-shell
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- shell.nix
+++ shell.nix
@@ -11,11 +11,17 @@ pkgs.mkShell {
     go_1_19
     golint
     kubectl
-    kubernetes-controller-tools
     kubernetes-helm
     gnumake
     minikube
     semver-tool
     yq-go
   ];
+  shellHook = ''
+    export GOPATH=$(pwd)/nix/.go
+    export TMPDIR=$(pwd)/nix/.tmp
+    export PATH=$GOPATH/bin:$PATH
+    go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.0
+    mkdir -p "$TMPDIR"
+  '';
 }
