d423456652b801fbe6e5b91e4320e38156b5c47f
Tiago Castro <tiagolobocastro@gmail.com>
2025-01-07 12:51:32 +0000

    build(nix-shell): add missing lvm packages to pure nix-shell
    Adds lvm2 and jq
    Also ensure sudo works in a pure shell
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -21,13 +21,29 @@ pkgs.mkShell {
     cacert
     util-linux
     musl
-  ] ++ pkgs.lib.optional (builtins.getEnv "IN_NIX_SHELL" == "pure") docker;
+    jq
+    lvm2
+  ] ++ pkgs.lib.optional (builtins.getEnv "IN_NIX_SHELL" == "pure") [ docker ];
   shellHook = ''
     export GOPATH=$(pwd)/nix/.go
     export GOCACHE=$(pwd)/nix/.go/cache
     export TMPDIR=$(pwd)/nix/.tmp
     export PATH=$GOPATH/bin:$PATH
     mkdir -p "$TMPDIR"
+
+    if [ "$IN_NIX_SHELL" = "pure" ]; then
+      # working sudo within a pure nix-shell
+      for sudo in /run/wrappers/bin/sudo /usr/bin/sudo /usr/local/bin/sudo /sbin/sudo /bin/sudo; do
+        mkdir -p $(pwd)/nix/bins
+        ln -sf $sudo $(pwd)/nix/bins/sudo
+        export PATH=$(pwd)/nix/bins:$PATH
+        break
+      done
+    else
+      rm $(pwd)/nix/bins/sudo 2>/dev/null || :
+      rmdir $(pwd)/nix/bins 2>/dev/null || :
+    fi
+
     make bootstrap
   '';
 }
